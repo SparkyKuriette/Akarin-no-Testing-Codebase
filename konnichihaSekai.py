252f28cdb98b1cafdba0d72f463c62375a22e393
Akarin Watt Kuriette <jaydensturgesnoble@gmail.com>
2025-07-22 19:21:40 +0800

    I'm writing a story. I plan on continuing this tomorrow. - Big Sparky

--- konnichihaSekai.py
+++ konnichihaSekai.py
@@ -10,3 +10,9 @@ z = x + y
 #This is an example of a comment and a push.
 if z := complex(10j + 20j):
     print(z) #This equals 30imaginary number.
+
+print("Now for a short story, named \"Maynesborough\".")
+input()
+print("Maynesborough. By Big Sparky. Licensed under the same license as this project. See LICENSE.md for details.")
+input()
+print("The town of Maynesborough, in the county of Maynes, in a country not too dissimilar to the United Kingdom, housed Teiya, a young girl, who had just met Alfo, a young man.")
