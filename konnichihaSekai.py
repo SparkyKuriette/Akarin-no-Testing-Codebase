7f44dae1b740681093e1a4edad0e35908fa31896
Akarin Watt Kuriette <jaydensturgesnoble@gmail.com>
2025-07-23 19:26:29 +0800

    A small story... I want to make a novel that's also a Python script.

--- konnichihaSekai.py
+++ konnichihaSekai.py
@@ -12,12 +12,12 @@ if z := complex(10j + 20j):
     print(z) #This equals 30imaginary number.
 
 #A small story.
-print("Now for a short story, named \"Maynesborough\" Press enter to continue.")
+print("Now for a short story, named \"Maynesborough\". Press enter to continue.")
 input()
 print("Maynesborough. By Big Sparky. Licensed under the same license as this project. See LICENSE.md for details. Press enter to continue.")
 input()
-print("The town of Maynesborough, in the county of Maynes, in a country not too dissimilar to the United Kingdom, lived Teiya, a young girl, who had just met Alfo, a young man.")
+print("In the town of Maynesborough, in the county of Maynes, in a country not too dissimilar to the United Kingdom, lived Teiya, a young girl, who had just met Alfo, a young man.")
 print("Teiya came home from work after she got a message that her mother had travelled to Mayscore, with her stating to Alfo, \"Ah, Alfo, me old mum had just travelled to Mayscore, and she's havin\' a rootin\' time, I bet ya!\"")
 print("(Press enter to continue)")
 input()
-print("\"Mayscore, eh?\"")
+print("\"Mayscore, eh?\" Alfo replied. \"Well, my mother is out of town, too!\"")
