6a8efde378e8ddebbf0d234bb3195502d1fde70c
Akarin Watt Kuriette <jaydensturgesnoble@gmail.com>
2025-07-23 19:22:31 +0800

    "Mayscore, a small story, written in Python."

--- konnichihaSekai.py
+++ konnichihaSekai.py
@@ -11,8 +11,13 @@ z = x + y
 if z := complex(10j + 20j):
     print(z) #This equals 30imaginary number.
 
-print("Now for a short story, named \"Maynesborough\".")
+#A small story.
+print("Now for a short story, named \"Maynesborough\" Press enter to continue.")
 input()
-print("Maynesborough. By Big Sparky. Licensed under the same license as this project. See LICENSE.md for details.")
+print("Maynesborough. By Big Sparky. Licensed under the same license as this project. See LICENSE.md for details. Press enter to continue.")
 input()
-print("The town of Maynesborough, in the county of Maynes, in a country not too dissimilar to the United Kingdom, housed Teiya, a young girl, who had just met Alfo, a young man.")
+print("The town of Maynesborough, in the county of Maynes, in a country not too dissimilar to the United Kingdom, lived Teiya, a young girl, who had just met Alfo, a young man.")
+print("Teiya came home from work after she got a message that her mother had travelled to Mayscore, with her stating to Alfo, \"Ah, Alfo, me old mum had just travelled to Mayscore, and she's havin\' a rootin\' time, I bet ya!\"")
+print("(Press enter to continue)")
+input()
+print("\"Mayscore, eh?\"")
